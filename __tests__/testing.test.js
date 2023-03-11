31914be66b5d48478c1d0dabdb476171cc30f99c
Brandon Spear <Bowdowntotheepicturkey@gmail.com>
2023-03-11 11:12:18 -0800

    PASS test of 'should display UsersAge(UA)'

--- __tests__/testing.test.js
+++ __tests__/testing.test.js
@@ -1,9 +1,16 @@
-import  from ;
+import Age from "../src/EarthAge.JS";
 
-describe('Triangle', () => {
+describe("Age ", () => {
+  let myAge;
 
-  test('it should ', () => {
-
-    expect().toEqual();
+  beforeEach(() => {
+    myAge = new Age(22);
   });
-});
+
+  describe("constructor", () => {
+    it("should display UsersAge(UA)", () => {
+      expect(myAge.age).toEqual(22);
+    });
+  });
+
+})
